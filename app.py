b1b9489d54695b5b3fc3bdccf8bed7e40421493a
Nukala Surendra <nukala.surendra@publicissapient.com>
2025-05-11 15:12:44 +0530

    Initial commit for WhatsApp bot

---
+++ app.py
@@ -0,0 +1,22 @@
+# app.py
+
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "WhatsApp-Salesforce Bot is Running!"
+
+@app.route('/webhook', methods=['GET', 'POST'])
+def webhook():
+    if request.method == 'GET':
+        return request.args.get("hub.challenge")
+    if request.method == 'POST':
+        print(request.json)  # this is where you process incoming WhatsApp messages
+        return "OK", 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
