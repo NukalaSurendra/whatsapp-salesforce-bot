d78dbfd2b269be6cba497985d8dd7f032fc55dfe
NukalaSurendra <surendra.n285@gmail.com>
2025-05-15 14:35:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -80,8 +80,8 @@ def send_message():
     payload = {
         "From": f"whatsapp:{TWILIO_WHATSAPP_NUMBER}",
         "To": f"whatsapp:{to}",
-        "ContentSid" : "HX3885bf7e1329631d9dadeb13ad2302c4"
-        "ContentVariables": message,
+        "ContentSid" : "HX3885bf7e1329631d9dadeb13ad2302c4",
+        "ContentVariables": message
         #"Body": message
     }
 
